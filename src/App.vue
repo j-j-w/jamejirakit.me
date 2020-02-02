8db0296d10b60a803545fa389872fe37a68b9dc0
Jirakit <jw.jirakit@gmail.com>
2020-02-02 08:33:53 +0700

    Add menu dropdown

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-page-container>
-      <router-view />
+    <q-btn class="absolute-top-right q-mr-lg q-mt-sm" flat round color="black" icon="menu">
+      <q-menu>
+        <q-list style="min-width: 100px">
+          <q-item clickable v-close-popup>
+            <q-item-section>New tab</q-item-section>
+          </q-item>
+          <q-item clickable v-close-popup>
+            <q-item-section>New incognito tab</q-item-section>
+          </q-item>
+        </q-list>
+      </q-menu>
+    </q-btn>
+    <q-page-container class="bg-green-1">
+      <transition name="fade" mode="in-out">
+        <router-view />
+      </transition>
     </q-page-container>
   </q-layout>
 </template>
@@ -9,17 +23,18 @@
 <script>
 
 export default {
-  name: 'LayoutDefault',
-  data () {
-    return {
-      leftDrawerOpen: false
-    }
-  }
+  name: 'app'
 }
 </script>
 
 <style scoped lang="css">
   body {
     font-family: 'Hack'
+  }
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .3s ease;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
   }
 </style>
